b45a15579263ff6d01953cf3cb57c05b2f99f05f
Rodrigo <rodriguinhosantanna@yahoo.com.br>
2024-10-31 19:42:42 -0300

    senha

--- src/main/java/com/example/tarefas/config/EncoderConfig.java
+++ src/main/java/com/example/tarefas/config/EncoderConfig.java
@@ -1,3 +1,5 @@
+// src/main/java/com/example/tarefas/config/EncoderConfig.java
+
 package com.example.tarefas.config;
 
 import org.springframework.context.annotation.Bean;
@@ -5,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+public class EncoderConfig {
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
